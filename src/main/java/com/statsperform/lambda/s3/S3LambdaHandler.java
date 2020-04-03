98b7038027753b992324a47679b29271d7a89354
d.hoefler <d.hoefler@pc121.Runningball.local>
2020-04-03 10:14:32 +0200

    IV-116 changed logging, added missing s3 dependency

--- src/main/java/com/statsperform/lambda/s3/S3LambdaHandler.java
+++ src/main/java/com/statsperform/lambda/s3/S3LambdaHandler.java
@@ -3,6 +3,9 @@ package com.statsperform.lambda.s3;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +21,37 @@ public class S3LambdaHandler implements RequestHandler<S3Event, String>
 		String result = "OK";
 		try
 		{
-			logger.info("S3 input received: " + input.toString()
-					+ ", Context data: AWS Request ID: " + context.getAwsRequestId()
+			List<S3EventNotificationRecord> records = input.getRecords();
+			logger.info("Number of S3 notification records: " + records.size()
+					+ ", context data: AWS Request ID: " + context.getAwsRequestId()
 					+ "Function name: " + context.getFunctionName()
 					+ ", Function version: " + context.getFunctionVersion()
 					+ ", invoked function urn:" + context.getInvokedFunctionArn()
 					+ ", log group name: " + context.getLogGroupName()
 					+ ", memory limit in mb: " + context.getMemoryLimitInMB()
-					+ ", remining time in millis: " + context.getRemainingTimeInMillis());
+					+ ", remaining time in millis: " + context.getRemainingTimeInMillis());
+			
+			for (S3EventNotificationRecord record : records)
+			{
+				logger.info("AWS Region: " + record.getAwsRegion()
+						+ ", event name:" + record.getEventName()
+						+ ", event source: " + record.getEventSource()
+						+ ", event version: " + record.getEventVersion()
+						+ ", eventNameAsEnum: " + record.getEventNameAsEnum().toString()
+						+ ", request param source ip: " + record.getRequestParameters().getSourceIPAddress()
+						+ ", response element xamzid2: " + record.getResponseElements().getxAmzId2()
+						+ ", response element amzrequestid: " + record.getResponseElements().getxAmzRequestId()
+						+ ", user identity principal id: " + record.getUserIdentity().getPrincipalId()
+						+ ", s3 configurationId: " + record.getS3().getConfigurationId()
+						+ ", s3 schema version: " + record.getS3().getS3SchemaVersion()
+						+ ", s3 bucket name: " + record.getS3().getBucket()
+						+ ", s3 object etag: " + record.getS3().getObject().geteTag()
+						+ ", s3 object key: " + record.getS3().getObject().getKey()
+						+ ", s3 object sequencer: " + record.getS3().getObject().getSequencer()
+						+ ", s3 object urldecoded key: "+ record.getS3().getObject().getUrlDecodedKey()
+						+ ", s3 object version: " + record.getS3().getObject().getVersionId()
+						+ ", s3 object size: " + record.getS3().getObject().getSizeAsLong());
+			}
 		}
 		catch (Exception e)
 		{
